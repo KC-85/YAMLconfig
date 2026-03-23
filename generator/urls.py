a75856f13d2498d0c60732523e55d77d1561c1d2
Kristian Cross <etamin1985@yahoo.com>
2026-03-23 19:07:15 +0000

    Add view function names to urls.py

--- generator/urls.py
+++ generator/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from django.views.generic import TemplateView
+
+from . import views
 
 
 app_name = "generator"
 
 urlpatterns = [
-	path("", TemplateView.as_view(template_name="generator/index.html"), name="index"),
+	path("", views.project_list, name="project_list"),
+	path("projects/create/", views.project_create, name="project_create"),
+	path("projects/<int:project_id>/", views.project_detail, name="project_detail"),
+	path("projects/<int:project_id>/edit/", views.project_edit, name="project_edit"),
+	path("projects/<int:project_id>/delete/", views.project_delete, name="project_delete"),
+	path("projects/<int:project_id>/services/create/", views.service_create, name="service_create"),
+	path("projects/<int:project_id>/services/<int:service_id>/edit/", views.service_edit, name="service_edit"),
+	path("projects/<int:project_id>/services/<int:service_id>/delete/", views.service_delete, name="service_delete"),
 ]
