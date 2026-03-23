f33f3df2db7dffc5de4ba2a0e98698da8671ada0
Kristian Cross <etamin1985@yahoo.com>
2026-03-23 18:22:35 +0000

    Add base view for deleting a service

--- generator/views.py
+++ generator/views.py
@@ -108,3 +108,15 @@ def service_edit(request: HttpRequest, project_id: int, service_id: int) -> Http
         form = ServiceForm(instance=service)
 
     return render(request, "generator/index.html", {"form": form, "project": project, "service": service})
+
+def service_delete(request: HttpRequest, project_id: int, service_id: int) -> HttpResponse:
+    project = get_object_or_404(ConfigProject, id=project_id)
+    service = get_object_or_404(Service, id=service_id, project=project)
+
+    if request.method == "POST":
+        service.delete()
+        messages.success(request, "Service deleted successfully.")
+
+        return redirect("generator:project_detail", project_id=project.id)
+
+    return render(request, "generator/index.html", {"project": project, "service": service})
