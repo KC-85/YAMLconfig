1c98df91becbf2304c06d7c9cdd50d6a25eafa3f
Kristian Cross <etamin1985@yahoo.com>
2026-03-23 20:05:39 +0000

    Add urls for options, networks and volumes

--- generator/urls.py
+++ generator/urls.py
@@ -14,4 +14,13 @@ urlpatterns = [
 	path("projects/<int:project_id>/services/create/", views.service_create, name="service_create"),
 	path("projects/<int:project_id>/services/<int:service_id>/edit/", views.service_edit, name="service_edit"),
 	path("projects/<int:project_id>/services/<int:service_id>/delete/", views.service_delete, name="service_delete"),
+	path("projects/<int:project_id>/options/create/", views.option_create, name="option_create"),
+	path("projects/<int:project_id>/options/<int:option_id>/edit/", views.option_edit, name="option_edit"),
+	path("projects/<int:project_id>/options/<int:option_id>/delete/", views.option_delete, name="option_delete"),
+	path("projects/<int:project_id>/networks/create/", views.network_create, name="network_create"),
+	path("projects/<int:project_id>/networks/<int:network_id>/edit/", views.network_edit, name="network_edit"),
+	path("projects/<int:project_id>/networks/<int:network_id>/delete/", views.network_delete, name="network_delete"),
+	path("projects/<int:project_id>/volumes/create/", views.volume_create, name="volume_create"),
+	path("projects/<int:project_id>/volumes/<int:volume_id>/edit/", views.volume_edit, name="volume_edit"),
+	path("projects/<int:project_id>/volumes/<int:volume_id>/delete/", views.volume_delete, name="volume_delete"),
 ]
